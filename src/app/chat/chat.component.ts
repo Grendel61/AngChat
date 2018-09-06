bdb8bee362db16f151e44b86f0a8bd75fa01665e
Ed Fullman <edfullman@gmail.com>
2018-09-06 17:24:19 -0500

    Github Pages

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewChecked, ElementRef, ViewChild } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from "angularfire2/firestore";
 import { Observable } from "rxjs";
 import { Message } from './../message.model';
@@ -9,14 +9,26 @@ import { Message } from './../message.model';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit{ 
+  @ViewChild('msgContainer') private messagesContainer: ElementRef;
   @Input() userAuth: string;
   messages: Observable<any[]>;
   private msgRef: AngularFirestoreCollection<Message>;
   constructor(private db: AngularFirestore) {
     this.msgRef = db.collection<Message>('messages', ref => ref.orderBy('timestamp'));
   }  
-  ngOnInit(): void {
+  ngOnInit() {
     this.messages = this.msgRef.valueChanges();
-
+    this.scrollToBottom();
   }
+
+  ngAfterViewChecked() {
+       this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    try {
+        this.messagesContainer.nativeElement.scrollTop = this.messagesContainer.nativeElement.scrollHeight;
+    } catch(err) { }
+  } 
+
 }
